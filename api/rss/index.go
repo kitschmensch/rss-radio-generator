e9cae16eb6b28addb294209113ea498e31a75d1c
Jake Mecham <jake@mecham.xyz>
2025-05-01 09:29:40 -0500

    Revert "Added GUID and PubDate"
    This reverts commit 315cad09922cb8c8b7332edc4f69aa4d0797c080.

--- api/rss/index.go
+++ api/rss/index.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"time"
 )
 
 var (
@@ -25,12 +24,6 @@ type RSS struct {
 	Channel Channel  `xml:"channel"`
 }
 
-type Guid struct {
-	XMLName     xml.Name `xml:"guid"`
-	IsPermaLink string   `xml:"isPermaLink,attr,omitempty"`
-	Value       string   `xml:",chardata"`
-}
-
 type Channel struct {
 	Title       string `xml:"title"`
 	Description string `xml:"description"`
@@ -41,8 +34,6 @@ type Channel struct {
 type Item struct {
 	Title     string     `xml:"title"`
 	Enclosure *Enclosure `xml:"enclosure"`
-	Guid      *Guid      `xml:"guid"`
-	PubDate   string     `xml:"pubDate,omitempty"`
 }
 
 type Enclosure struct {
@@ -130,15 +121,9 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 
 	// Build RSS items
 	var items []Item
-	itemPubDate := time.Now().Format(time.RFC1123Z)
 	for _, station := range formData.Stations {
 		items = append(items, Item{
 			Title: station.Title,
-			Guid: &Guid{ // Add GUID using the station URL
-				IsPermaLink: "false",
-				Value:       station.URL,
-			},
-			PubDate: itemPubDate,
 			Enclosure: &Enclosure{
 				URL:    station.URL,
 				Length: "0",
